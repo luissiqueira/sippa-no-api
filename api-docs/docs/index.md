a6b228466d7058bcc614d339750b3001195b8211
Luis Siqueira <luissiqueirajr@gmail.com>
2016-10-19 19:39:32 -0300

    NEW: inciado doc

---
+++ api-docs/docs/index.md
@@ -0,0 +1,4 @@
+# SIPPA-NO-API
+
+SIPPA-NO-API é uma API não oficial do sistema [SIPPA](https://sistemas.quixada.ufc.br/apps/sippa/)
+utilizado pela UFC-Quixadá.
--- core/views.py
+++ core/views.py
@@ -1,12 +1,17 @@
+import msgpack
+
 from django.views.decorators.http import require_GET, require_POST
 
 from business.wrapper import SIPPAWrapper
-from django.http.response import JsonResponse
+from django.http.response import JsonResponse, HttpResponse
 
 
 @require_GET
 def start_login(request):
-    return JsonResponse(SIPPAWrapper().start_auth())
+    response = SIPPAWrapper().start_auth()
+    response_format = request.GET.get('format', 'json')
+
+    return dict_to_response(response, response_format)
 
 
 @require_POST
@@ -19,5 +24,13 @@ def process_login(request):
     captcha_value = request.POST.get('captcha_value')
 
     response = sippa.run(session=session, login=login, password=password, captcha_value=captcha_value)
+    response_format = request.POST.get('format', 'json')
 
-    return JsonResponse(response)
+    return dict_to_response(response, response_format)
+
+
+def dict_to_response(response, response_format):
+    if response_format == 'msgpack':
+        return HttpResponse(msgpack.packb(response))
+    else:
+        return JsonResponse(response)
